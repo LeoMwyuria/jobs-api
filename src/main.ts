f2322fe3870ddfe994222307eb1d5bfb6fd26981
LeoMwyuria <chargeishvililevani@gmail.com>
2024-11-25 17:44:10 +0400

    port fix

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,9 @@ async function bootstrap() {
   app.enableCors();
   
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT') || 3000;
+  const port = process.env.PORT || configService.get('PORT') || 3000;
   
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
